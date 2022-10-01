172f69d96d3ef3b81dee26c222b73b2a651c4e3d
JIRATEEP JANKHAEW <6210110045@psu.ac.th>
2022-10-01 21:01:10 +0700

    two_characters func & TestTwoCharacters

--- src/two_characters.py
+++ src/two_characters.py
@@ -1,2 +1,20 @@
 def two_characters(text):
-    return
+        list_set = list(set(text))
+        len_max = 0
+        for i in range(len(list_set)):
+            for j in range(i+1,len(list_set)):
+                prev = '0'
+                success = True
+                cur_len = 0
+                for e in text:
+                    if e == list_set[i] or e == list_set[j]:
+                        cur = e
+                        if cur == prev:
+                                success = False
+                                break
+                        else:
+                            prev = cur
+                            cur_len += 1
+                if success and cur_len > len_max:
+                    len_max = cur_len
+        return len_max
--- tests/test_two_characters.py
+++ tests/test_two_characters.py
@@ -1,2 +1,9 @@
 import unittest
 from src.two_characters import two_characters
+
+class TestTwoCharacters(unittest.TestCase):
+    def test_give_beabeefeab_should_5(self):
+        text = 'beabeefeab'
+        excepted_result = 5
+        result = two_characters(text)
+        self.assertEqual(result, excepted_result)
