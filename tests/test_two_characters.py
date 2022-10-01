fb23ab6e1a66ac1d5e178af2cdb187e71e942e67
JIRATEEP JANKHAEW <6210110045@psu.ac.th>
2022-10-01 21:02:21 +0700

    TestTwoCharacters add 1 test case

--- tests/test_two_characters.py
+++ tests/test_two_characters.py
@@ -7,3 +7,9 @@ class TestTwoCharacters(unittest.TestCase):
         excepted_result = 5
         result = two_characters(text)
         self.assertEqual(result, excepted_result)
+
+    def test_give_asdcbsdcagfsdbgdfanfghbsfdab_should_8(self):
+        text = 'asdcbsdcagfsdbgdfanfghbsfdab'
+        excepted_result = 8
+        result = two_characters(text)
+        self.assertEqual(result, excepted_result)
