021e3e7f751488cfc3d18c25b090b6b488ca35a5
JIRATEEP JANKHAEW <6210110045@psu.ac.th>
2022-10-01 15:51:50 +0700

    alternating_characters_test_give_AAABBB_should_4

--- tests/test_alternating_characters.py
+++ tests/test_alternating_characters.py
@@ -25,3 +25,9 @@ class TestAlternatingCharacters(unittest.TestCase):
         excepted_result = 0
         result = alternating_characters(text)
         self.assertEqual(excepted_result, result)
+
+    def test_give_AAABBB_should_0(self):
+        text = 'AAABBB'
+        excepted_result = 4
+        result = alternating_characters(text)
+        self.assertEqual(excepted_result, result)
