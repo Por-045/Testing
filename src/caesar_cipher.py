c8543e23d8543ddb241f7e2348870b9eee3494ea
JIRATEEP JANKHAEW <6210110045@psu.ac.th>
2022-10-01 20:29:24 +0700

    TestCaesarCipher

--- src/caesar_cipher.py
+++ src/caesar_cipher.py
@@ -1,2 +1,19 @@
-def caesar_cipher():
-    return
+def caesar_cipher(text, rotation):
+
+    rotation = rotation % 26
+
+    encrypt_text = ""
+
+    for i in text:
+        if ord(i) >= 97 and ord(i) <= (122-rotation):
+            encrypt_text += chr(ord(i) + rotation)
+        elif ord(i) > (122 - rotation) and ord(i) <= 122:
+            encrypt_text += chr(ord(i) - 26 + rotation)
+        elif ord(i) >= 65 and ord(i) <= (90-rotation):
+            encrypt_text += chr(ord(i) + rotation)
+        elif ord(i) > (90 - rotation) and ord(i) <= 90:
+            encrypt_text += chr(ord(i) - 26 + rotation)
+        else:
+            encrypt_text+=i
+
+    return encrypt_text
--- tests/test_caesar_cipher.py
+++ tests/test_caesar_cipher.py
@@ -1,2 +1,39 @@
 import unittest
 from src.caesar_cipher import caesar_cipher
+
+class TestCaesarCipher(unittest.TestCase):
+    def test_give_middle_outz_should_okffng_qwvb(self):
+        text = 'middle_outz'
+        rotation = 2
+        excepted_result = 'okffng_qwvb'
+        result = caesar_cipher(text, rotation)
+        self.assertEqual(result, excepted_result)
+
+    def test_give_abcdefghijklmnopqrstuvwxyz_should_defghijklmnopqrstuvwxyzabc(self):
+        text = 'abcdefghijklmnopqrstuvwxyz'
+        rotation = 3
+        excepted_result = 'defghijklmnopqrstuvwxyzabc'
+        result = caesar_cipher(text, rotation)
+        self.assertEqual(result, excepted_result)
+    
+    def test_give_rgnoutghaiogauiopgzsdklf_should_rgnoutghaiogauiopgzsdklfc(self):
+        text = 'rgnoutghaiogauiopgzsdklf'
+        rotation = 26
+        excepted_result = 'rgnoutghaiogauiopgzsdklf'
+        result = caesar_cipher(text, rotation)
+        self.assertEqual(result, excepted_result)
+
+    def test_give_jirateep_should_gfoxqbbm(self):
+        text = 'jirateep'
+        rotation = -3
+        excepted_result = 'gfoxqbbm'
+        result = caesar_cipher(text, rotation)
+        self.assertEqual(result, excepted_result)
+
+    def test_give_JIRATEEP_should_GFOXQBBM(self):
+        text = 'JIRATEEP'
+        rotation = -3
+        excepted_result = 'GFOXQBBM'
+        result = caesar_cipher(text, rotation)
+        self.assertEqual(result, excepted_result)
+
